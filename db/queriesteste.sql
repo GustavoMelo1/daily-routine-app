d324a15b8f0252170a796205d272a6535d1b3e64
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-07-06 19:35:16 -0300

    chore: add practice queries (aggregate functions), test only

--- db/queriesteste.sql
+++ db/queriesteste.sql
@@ -4,3 +4,13 @@ INNER JOIN tarefas ON dias.id = tarefas.dia_id;
 SELECT descricao, cumprida FROM tarefas
 WHERE cumprida = 0
 ORDER BY descricao ASC;
+
+SELECT cumprida, COUNT(*)
+FROM tarefas
+GROUP BY cumprida;
+
+SELECT SUM(minutos_estudados) FROM dias;
+
+SELECT AVG(minutos_estudados) FROM dias;
+
+SELECT MAX(minutos_estudados), MIN(minutos_estudados) FROM dias; 
