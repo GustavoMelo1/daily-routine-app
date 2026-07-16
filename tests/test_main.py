677a01e39cf44e108fea340d66bb5accd7c115a5
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-07-15 22:22:12 -0300

    chore: reorder tests by resource and http verb

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,16 +6,6 @@ client = TestClient(app)
 def test_get_dia_existente():
     resposta = client.get("/dias/2026-07-02")
     assert resposta.status_code == 200
-    
-def test_criar_dia_duplicado():
-    resposta = client.post("/dias", json={
-        "data": "2026-07-02",
-        "minutos_estudados": 10,
-        "frase_do_dia": "teste",
-        "autor_frase": "teste",
-        "tipo": "normal"
-    })
-    assert resposta.status_code == 400
 
 def test_criar_dia_sucesso():
     resposta = client.post("/dias", json={
@@ -29,15 +19,15 @@ def test_criar_dia_sucesso():
 
     client.delete("/dias/2026-08-01")
 
-def test_criar_tarefa_sucesso():
-    resposta = client.post("/tarefas", json={
-        "dia_id": 1,
-        "descricao": "teste",
-        "cumprida": 1
+def test_criar_dia_duplicado():
+    resposta = client.post("/dias", json={
+        "data": "2026-07-02",
+        "minutos_estudados": 10,
+        "frase_do_dia": "teste",
+        "autor_frase": "teste",
+        "tipo": "normal"
     })
-    assert resposta.status_code == 200
-
-    client.delete(f"/tarefas/{resposta.json()['id']}")
+    assert resposta.status_code == 400
 
 def test_deletar_dia_sucesso():
     client.post("/dias", json={
@@ -55,3 +45,13 @@ def test_deletar_dia_inexistente():
     resposta = client.delete("/dias/2027-10-01")
 
     assert resposta.status_code == 404
+
+def test_criar_tarefa_sucesso():
+    resposta = client.post("/tarefas", json={
+        "dia_id": 1,
+        "descricao": "teste",
+        "cumprida": 1
+    })
+    assert resposta.status_code == 200
+
+    client.delete(f"/tarefas/{resposta.json()['id']}")
