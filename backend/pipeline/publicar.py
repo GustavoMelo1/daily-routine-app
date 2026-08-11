1136ed1be84c5d0fcee0eb48e2e09252c2e90b5d
Gustavo <gustavomelo.oliveira@hotmail.com>
2026-08-10 21:13:32 -0300

    feat: adding list of json dictionaries in OCR

---
+++ backend/pipeline/publicar.py
@@ -0,0 +1,14 @@
+import requests
+
+
+json_dias = {
+    "data" : "2026-08-10",
+    "minutos_estudados": 45,
+    "frase_do_dia": "Amar e deixar ir " ,
+    "autor_frase": "Gustavo Melo",
+    "tipo": "Normal"
+}
+
+json_tarefas = [
+    {"descricao": "studar Python", "cumprida": 1},
+]
